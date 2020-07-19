56123493efabca3f3287585c890f62143e1856e1
Jaicke <jaicke-santos@outlook.com>
2020-07-19 02:58:50 -0300

    Setup webpack, webpack-dev-server and module exercises

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import ClasseUsuario from './functions'
+import { idade } from './functions'
+import { Usuario, idade as idadeUsuario } from './functions'
+
+ClasseUsuario.info()
+console.log(idade)
+console.log(idadeUsuario)
